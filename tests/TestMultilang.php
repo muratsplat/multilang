b2ec93670acb864c0b2a047bc9619d4125dd54c5
Murat Ödünç <murat.asya@gmail.com>
2014-12-20 20:18:49 +0200

    new test file includes models and a test for Multilang

---
+++ tests/TestMultilang.php
@@ -0,0 +1,40 @@
+<?php namespace Muratsplat\Multilang\Tests;
+
+use Muratsplat\Multilang\Picker;
+use Muratsplat\Multilang\Tests\Base;
+use Muratsplat\Multilang\Element;
+use Muratsplat\Multilang\MultiLang;
+use Illuminate\Support\Collection;
+use Illuminate\Config\Repository as Config;
+use Muratsplat\Multilang\Tests\Model\Content;
+use \Mockery as m;
+
+
+/**
+ * a test class for \Muratsplat\Multilang\MultiLang
+ *
+ * @author Murat Ödünç <murat.asya@gmail.com>
+ * @copyright (c) 2015, Murat Ödünç
+ * @link https://github.com/muratsplat/multilang Project Page
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPLv3 
+ */
+class TestMultilang extends Base {   
+    
+    public function tearDown() {
+        parent::tearDown();
+        
+        m::close();
+    }
+    
+    public function testFirstSimple() {
+       
+        $mockedConfig = m::mock('Illuminate\Config\Repository','Illuminate\Config\LoaderInterface');             
+                
+        $obj = new MultiLang(new Picker(new Collection(), new Element()), new Content,$mockedConfig);
+    }
+    
+    
+    
+    
+    
+}
---
+++ tests/model/Models.php
@@ -0,0 +1,30 @@
+<?php namespace Muratsplat\Multilang\Tests\Model;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *  Simple Models For tests
+ * 
+ * @author Murat Ödünç <murat.asya@gmail.com>
+ * @copyright (c) 2015, Murat Ödünç
+ * @link https://github.com/muratsplat/multilang Project Page
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPLv3 
+ */
+
+/**
+ * Simple Main Model
+ */
+class Content extends Model {
+    //put your code here
+}
+
+
+/**
+ * ContantLang  will be Content's multi languages model.
+ */
+class ContentLang extends Model {
+    //put your code here
+}
+
+
