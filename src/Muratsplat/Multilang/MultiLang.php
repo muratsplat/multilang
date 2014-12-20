674a0636afa0854bc094264501b4f01467ae8458
Murat Ödünç <murat.asya@gmail.com>
2014-12-20 20:17:10 +0200

    added Mocker/Mock for advanced test and changes

--- src/Muratsplat/Multilang/MultiLang.php
+++ src/Muratsplat/Multilang/MultiLang.php
@@ -1,7 +1,8 @@
 <?php namespace Muratsplat\Multilang;
 
-//use Illuminate\Support\Collection;
-//use Muratsplat\Multilang\Element;
+use Illuminate\Database\Eloquent\Model;
+use Muratsplat\Multilang\Picker;
+use Illuminate\Config\Repository as Config;
 //use Muratsplat\Multilang\Exceptions\ElementUndefinedProperty;
 //use Muratsplat\Multilang\Exceptions\PickerUnknownError;
 //use Muratsplat\Multilang\Exceptions\PickerError;
@@ -18,5 +19,57 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPLv3 
  */
 class MultiLang {
-    //put your code here
+    
+   /**
+    * Main Model  
+    * 
+    * @var Illuminate\Database\Eloquent\Model
+    */
+    private $mainModel;
+    
+    /**
+     * Language Model
+     * 
+     * @var Illuminate\Database\Eloquent\Model 
+     */
+    private $langModel;
+    
+    /**
+     * Picker Object
+     * 
+     * @var Muratsplat\Multilang\Picker 
+     */
+    private $picker;
+    
+    /**
+     * an prefix to pick multi-language elements up
+     * 
+     * @var string 
+     */
+    private $prefix;
+    
+    /** 
+     * Laravel Config Object
+     *
+     * @var Illuminate\Config\Repositor 
+     */
+    private $config;
+    
+    
+
+
+        public function __construct(Picker $picker, Model $model, Config $config) {
+            
+            $this->picker = $picker;
+            
+            $this->mainModel = $model;
+            
+            $this->config= $config;
+            
+            
+    
+            
+        }
+        
+        
 }
