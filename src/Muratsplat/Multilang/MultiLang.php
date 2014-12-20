81bac3051fc098b95af1fa993f0aacc74f06eda5
Murat Ödünç <murat.asya@gmail.com>
2014-12-20 16:27:55 +0200

    add new class Multilang. Probably it is major..

---
+++ src/Muratsplat/Multilang/MultiLang.php
@@ -0,0 +1,22 @@
+<?php namespace Muratsplat\Multilang;
+
+//use Illuminate\Support\Collection;
+//use Muratsplat\Multilang\Element;
+//use Muratsplat\Multilang\Exceptions\ElementUndefinedProperty;
+//use Muratsplat\Multilang\Exceptions\PickerUnknownError;
+//use Muratsplat\Multilang\Exceptions\PickerError;
+
+/**
+ * MultiLang Class
+ * 
+ * The class make be easy to manage multi language content when
+ * it thinks all CRUD process by working on ORM.
+ * 
+ * @author Murat Ödünç <murat.asya@gmail.com>
+ * @copyright (c) 2015, Murat Ödünç
+ * @link https://github.com/muratsplat/multilang Project Page
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPLv3 
+ */
+class MultiLang {
+    //put your code here
+}
