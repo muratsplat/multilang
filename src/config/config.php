013ca7aee05aa19bb7c56deb208391c79c637e59
Murat Ödünç <murat.asya@gmail.com>
2015-04-11 00:20:34 +0300

    chenges default

--- src/config/config.php
+++ src/config/config.php
@@ -50,6 +50,6 @@ return array(
          * The value must be integer and positive number.
          * Time duration is minute.
          */
-        'rememberTime'      => 10,
+        'rememberTime'      => 120,
     
     );
